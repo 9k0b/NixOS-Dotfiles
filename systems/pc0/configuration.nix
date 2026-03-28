b6d53ea8e5093ee6bed56954f0e3cd7efa931ae4
9k0b <jakob.fuenderich@gmail.com>
2026-03-28 18:17:23 +0100

    fixing path§

--- systems/pc0/configuration.nix
+++ systems/pc0/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../nixos/default.nix
+      ../../modules/nixos/default.nix
     ];
 
   nix.settings.experimental-features = [
