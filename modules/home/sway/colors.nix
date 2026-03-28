d7ef950883083acabe5cba2ef42399d4958ad846
9k0b <jakob.fuenderich@gmail.com>
2026-03-28 14:53:33 +0100

    colors are different

--- modules/home/sway/colors.nix
+++ modules/home/sway/colors.nix
@@ -9,9 +9,9 @@
   };
   focusedInactive = {
      background = "#24283b";
-     border = "#bb9af7";
-     childBorder = "#bb9af7";
-     indicator = "#bb9af7";
+     border = "#9ece6a";
+     childBorder = "#9ece6a";
+     indicator = "#9ece6a";
      text = "#c0caf5";
   };
   unfocused = {
