cd72bbf1ad2718d363a1972701a59c2d0786a5b1
9k0b <jakob.fuenderich@gmail.com>
2026-03-28 14:21:04 +0100

    fixed startup

---
+++ modules/home/sway/auto-start.nix
@@ -0,0 +1,13 @@
+[
+  {command = "swaymsg 'workspace 1, exec foot'";}
+  {command = "swaymsg 'workspace 2, exec zen-browser'";}
+  {command = "swaymsg 'workspace 3, exec foot yazi'";}
+  {command = "swaymsg 'workspace 3, exec foot spotify_player'";}
+  {command = "swaymsg 'workspace 4, exec keepassxc'";}
+  {command = "swaymsg 'workspace 4, exec foot btop'";}
+  {command = "swaymsg 'workspace 5, exec devpod-desktop'";}
+  {command = "swaymsg 'workspace 5, exec podman-desktop'";}
+  {command = "swaymsg 'workspace 6, exec helium-browser'";}
+  {command = "swaymsg 'exec waybar'";}
+  {command = "swaymsg 'exec swww'";}
+]
