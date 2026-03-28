83fbb5e65a2212899eb7bca9a4ce6331de32bc05
9k0b <jakob.fuenderich@gmail.com>
2026-03-28 20:25:30 +0100

    ueof

---
+++ modules/home/misc/ssh-agent.nix
@@ -0,0 +1,15 @@
+{pkgs, lib, config, ... }:
+
+{
+
+  options = {
+    sshAgent.enable =
+      lib.mkEnableOption "enable ssh-agent";
+  };
+
+  config = lib.mkIf config.sshAgent.enable {
+    services.ssh-agent = {
+      enable = true;
+    };
+  };
+}
