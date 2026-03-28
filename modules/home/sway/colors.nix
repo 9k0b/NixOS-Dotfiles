1c3de6d2c642f6850fcc004d9a2f247f06f1f886
9k0b <jakob.fuenderich@gmail.com>
2026-03-28 14:49:21 +0100

    hopefully fixed

---
+++ modules/home/sway/colors.nix
@@ -0,0 +1,25 @@
+{
+
+  focused = {
+    background = "#24283b";
+    border = "#bb9af7";
+    childBorder = "#bb9af7";
+    indicator = "#bb9af7";
+    text = "#c0caf5";
+  };
+  focusedInactive = {
+     background = "#24283b";
+     border = "#bb9af7";
+     childBorder = "#bb9af7";
+     indicator = "#bb9af7";
+     text = "#c0caf5";
+  };
+  unfocused = {
+    background = "#24283b";
+    border = "#565f89";
+    childBorder = "#565f89";
+    indicator = "#565f89";
+    text = "#9aa5ce";
+  };
+
+}
