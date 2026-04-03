4f62d2ce555844eff7a1de50e35a081eae9fe121
9k0b <jakob.fuenderich@gmail.com>
2026-04-03 22:06:25 +0200

    Scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# NixOS Config
+
+This Repository contains my entire NixOS Config
---
+++ scripts/git/add_commit.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+
+commit_message=""
+git_repo_create_y_or_no=""
+
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    git add -A
+    read -p "Enter Commit Message: " commit_message
+    git commit -m "$commit_message"
+else
+  read -p "Not in a Git Repo initialize (y/n)"
+  if [[ "$git_repo_create_y_or_no" == "y" || "$git_repo_create_y_or_no" == "Y" ]]; then
+    git init
+    echo "Initialized Git Repo"
+  else
+    echo "Canceled"
+  fi
+fi
