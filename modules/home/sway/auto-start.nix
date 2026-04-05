00663b75f214f8c7e35ac1ba9ee66050469b3ea9
9k0b <jakob.fuenderich@gmail.com>
2026-04-05 21:06:24 +0200

    waybar start and new autostart apps

--- modules/home/sway/auto-start.nix
+++ modules/home/sway/auto-start.nix
@@ -1,13 +1,10 @@
 [
   {command = "swaymsg 'workspace 1, exec foot'";}
   {command = "swaymsg 'workspace 2, exec zen-browser'";}
-  {command = "swaymsg 'workspace 3, exec foot yazi'";}
-  {command = "swaymsg 'workspace 3, exec foot spotify_player'";}
+  {command = "swaymsg 'workspace 3, exec zeditor'";}
   {command = "swaymsg 'workspace 4, exec keepassxc'";}
-  {command = "swaymsg 'workspace 4, exec foot btop'";}
-  {command = "swaymsg 'workspace 5, exec devpod-desktop'";}
-  {command = "swaymsg 'workspace 5, exec podman-desktop'";}
-  {command = "swaymsg 'workspace 6, exec helium-browser'";}
+  {command = "swaymsg 'workspace 5, exec foot spotify_player'";}
+  {command = "swaymsg 'workspace 5, exec foot btop'";}
   {command = "swaymsg 'exec waybar'";}
   {command = "awww-daemon &";}
   {command = "awww img ~/.nixos/images/wallpapers/tokyo_night_cafe.png";}
